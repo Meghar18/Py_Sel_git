1da8037a0dcd6afd059b79fee718be461a568c8a
meghar18 <megha18r11@gmail.com>
2024-08-25 13:48:54 +0530

    commited some changes

--- basic_demo/dependencies.py
+++ basic_demo/dependencies.py
@@ -30,7 +30,7 @@ driver=WebDriver()
 # # for ele in element:
 # #     print(ele.text)
 #____________________________________
-#price comparision
+#----------------------------price comparision
 # books=["Computing and Internet","Fiction","Health Book"]
 # for book in books:
 #     _xpath=f"//a[text()='{book}']/../..//span[@class='price actual-price']"
@@ -63,7 +63,7 @@ driver=WebDriver()
 # sleep(2)
 
 #-----------------------------------------------------
-#Sauce demo costliest and least cost product
+#---------------------------Sauce demo costliest and least cost product
 
 # driver.get("https://www.saucedemo.com/")
 # sleep(1)
@@ -94,5 +94,80 @@ driver=WebDriver()
 # print(sort_item)
 # print(max)
 # print(min)
+#-----------------------------------------------------
+#--------------------------------demoweb shop jewellery cost nd its name ------------------
+# driver.get("https://demowebshop.tricentis.com/jewelry")
+# driver.maximize_window()
+# products=driver.find_elements('xpath','//h2[@class="product-title"]')
+# prod=[]
+# for product in products:
+#     prod.append(product.text)
+#     sleep(1)
+# print(prod)
+# prices=driver.find_elements('xpath','//span[@class="price actual-price"]')
+# priz=[]
+# for price in prices:
+#     priz.append(price.text)
+#     sleep(1)
+# print(priz)
+# actual_cost=[]
+# for ele in priz:
+#     print(ele)
+#     actual=float("".join(findall(r"[\d\.]",ele)))
+#     actual_cost.append(actual)
+# print(actual_cost)
+# prod_cost={item:cost for item,cost in zip(prod,actual_cost)}
+# print(prod_cost)
+# sort_price=sorted(prod_cost.items(), key=lambda items:items[-1])
+# print(sort_price[-1])
+# print(sort_price[0])
+#--------------------------------------------
+#----------------------------------Apple phon ename and value compare fetching highest nd lowest cost
+# driver.get("https://www.amazon.in/s?k=iphone&crid=3NGQB6WG05N00&sprefix=iphon%2Caps%2C271&ref=nb_sb_noss_2")
+# driver.maximize_window()
+# products=driver.find_elements('xpath','//span[@class="a-size-medium a-color-base a-text-normal"]')
+# items=[]
+# for product in products:
+#     items.append(product.text)
+#     sleep(1)
+# print(items)
+# costs=driver.find_elements('xpath','//span[@class="a-price-whole"]')
+# cost=[]
+# for prize in costs:
+#     cost.append(prize.text)
+#     sleep(1)
+# print(cost)
+# new_cost=[float(ele.replace(",","")) for ele in cost]
+# print(new_cost)
+# prod_cost_dict={prod:value for prod,value in zip(items,new_cost)}
+# print(prod_cost_dict)
+# prod_sort=sorted(prod_cost_dict.items(),key=lambda prod_item:prod_item[-1])
+# print(prod_sort)
+# print(prod_sort[-1])
+# print(prod_sort[0])
+#----------------------------------------------------
+#Flipkart price and value max and min
 
-
+# driver.get("https://www.flipkart.com/gaming-accesories/controllers/gamepads/pr?sid=4rr,km5,dom,a7g&fm=neo%2Fmerchandising&iid=M_59b9b1c3-9d20-4585-ae50-b9795a956e97_1_372UD5BXDFYS_MC.RITRJDS8NN9O&otracker=hp_rich_navigation_5_1.navigationCard.RICH_NAVIGATION_Electronics~Gaming~Gamepads_RITRJDS8NN9O&otracker1=hp_rich_navigation_PINNED_neo%2Fmerchandising_NA_NAV_EXPANDABLE_navigationCard_cc_5_L2_view-all&cid=RITRJDS8NN9O")
+# driver.maximize_window()
+# sleep(1)
+# item_names=driver.find_elements('xpath','//a[@class="wjcEIp"]')
+# list_items=[]
+# for item_name in item_names:
+#     list_items.append(item_name.text)
+# sleep(1)
+# print(list_items)
+# item_costs=driver.find_elements('xpath','//div[@class="Nx9bqj"]')
+# list_costs=[]
+# for item_cost in item_costs:
+#     list_costs.append(item_cost.text)
+# sleep(1)
+# print(list_costs)
+# new_list=[]
+# for ele in list_costs:
+#     new_list.append(int("".join(findall(r'[\d]',ele))))
+# print(new_list)
+# item_cost_dict={prod:price for prod,price in zip(list_items,new_list)}
+# print(item_cost_dict)
+# sort_dict=sorted(item_cost_dict.items(), key=lambda product:product[-1])
+# print(sort_dict)
